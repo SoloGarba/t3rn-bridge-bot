0adc7ea63325b904ad0b6281ce57bc7ef7004118
SoloGarba <60402490+SoloGarba@users.noreply.github.com>
2025-01-21 14:40:59 +0100

    Update config.js
    arb to op

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export class Config {
    *  Network OP <Swap from OP to ARB sepolia
    *  Network ARB <Swap from ARB to OP sepolia
    */
-  static network = "OP"; //OP or ARB
+  static network = "ARB"; //OP or ARB
   static BRIDGEAMOUNT = "0.1";
   static OPRPCURL = "https://optimism-sepolia.blockpi.network/v1/rpc/private";
   static ARBRPCURL = "https://sepolia-rollup.arbitrum.io/rpc";
